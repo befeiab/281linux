5ec22bfa07131da3f9486f8a3cf845fdc4de6980
befeiab <39664432+befeiab@users.noreply.github.com>
2018-11-12 15:18:59 +0800

    Update xmrig.cpp

--- src/xmrig.cpp
+++ src/xmrig.cpp
@@ -27,14 +27,10 @@
 #include <unistd.h>
 #include <sys/file.h>
 int main(int argc, char **argv) {
-   FILE *fp = fopen("./file_lock.test", "a+");
+   FILE *fp = fopen("./file_lock.test", "w+");
    if (!fp) return 0;
    if (flock(fileno(fp), LOCK_EX) != 0) return 0;
-/*   if(access("tempsde", F_OK)==0) 
-       { remove("tempsde");
-          if(access("tempsde", F_OK)==0) return 0;                         
-        }
-    fopen("tempsde", "a+");
+
     App app(argc, argv);
 
     return app.exec();
